72e395e004ff6b3805df0d72a812c57e06293b44
Manuel RUSSO <manuel.russo@worldline.com>
2023-03-23 23:07:36 +0100

    Try sharing data

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+
+var num = 0;
+
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  num++;
+  res.send("Express on Vercel: " + num);
 });
 app.listen(5000, () => {
   console.log("Running on port 5000.");
