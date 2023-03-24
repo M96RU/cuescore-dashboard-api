8e6c06bca57ecc48259202441e16f9140290cbbb
Manuel RUSSO <manuel.russo@worldline.com>
2023-03-24 15:24:33 +0100

    Create and get match data

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-var num = 0;
+const cache = require('map-expire');
 
-app.get("/", (req, res) => {
-  num++;
-  res.send("Express on Vercel: " + num);
+const duration = 10 * 60 * 1000; // 10 minutes
+
+app.get('/', (req, res) => {
+    res.send("Express on Vercel");
 });
+
+app.get('/api/match/:id', (req, res) => {
+    const match = cache.get(req.params.id);
+    res.send(match);
+});
+
+app.post('/api/match', function (req, res) {
+    cache.set(req.body.id, req.body, duration);
+    res.send(req.body);
+})
+
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
+    console.log("Running on port 5000.");
 });
 // Export the Express API
 module.exports = app;
