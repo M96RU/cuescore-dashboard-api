442e47f27e5da78667dbf4be5ec4adf2bf6fd9f7
M96RU <m96ru.info@gmail.com>
2024-10-07 22:30:43 +0200

    clean

--- cuescore/index.js
+++ cuescore/index.js
@@ -1,40 +1,20 @@
 const proxy = require('./proxy');
 const results = require('./results');
 
+const baseUrl = '/api/cuescore'
+
 const init = (app) => {
 
-    app.get('/api/cuescore', (req, res) => {
-        const response = {
-            tournaments: [
-                {
-                    id: "49944664",
-                    organization: "lbara",
-                    type: "mixte"
-                }, {
-                    id: "49997359",
-                    organization: "lbara",
-                    type: "women"
-                }
-            ]
-        }
-        res.send(response);
-    });
-
-    app.get('/api/cuescore/live', (req, res) => {
-        const data = integration.getData();
-        res.send({
-            matches: Object.values(data.matches).filter(match => match.status === 'playing'),
-        });
-    });
-
-    app.get('/api/cuescore/matches', async (req, res) => {
+    app.get(baseUrl + '/matches', async (req, res) => {
+        // live: match.status === 'playing'
         const matches = await proxy.getMatches();
         res.send({
             proxy: matches,
             matches: results.getMatches()
         });
     });
-    app.get('/api/cuescore/players', async (req, res) => {
+
+    app.get(baseUrl + '/players', async (req, res) => {
         const players = await proxy.getPlayers();
         res.send({
             proxy: players,
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cache = require('map-expire');
 const duration = 10 * 60 * 1000; // 10 minutes
 
 app.get('/', (req, res) => {
-    res.send("Express on Vercel with CORS");
+    res.send("{'status': 'OK'}");
 });
 
 app.get('/api/match/:id', (req, res) => {
