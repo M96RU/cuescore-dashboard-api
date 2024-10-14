e8d44af3a8a69e333c44818bfde80c3a9214d73d
M96RU <m96ru.info@gmail.com>
2024-10-14 22:46:19 +0200

    add scorer url

--- cuescore/model/match.js
+++ cuescore/model/match.js
@@ -6,6 +6,7 @@ module.exports = class Match {
     organization;
     draw;
     duration;
+    scorerUrl;
 
     // Match
     id; // pk
--- cuescore/proxy/index.js
+++ cuescore/proxy/index.js
@@ -4,21 +4,44 @@ Player = require('./../model/player');
 
 const labels = {
     // Organizations
-    'ffb' : 'FF Billard',
-    'lbara' : 'LBARA',
+    'ffb': 'FF Billard',
+    'lbara': 'LBARA',
 
     // Common draws
     'mixte': 'Mixte',
-    'women' : 'Féminin',
+    'women': 'Féminin',
     'juniors': 'Jeunes',
 
     // FFB draws
-    'bbm' : 'BBM',
+    'bbm': 'BBM',
 
     // LBARA draws
     'prestige': 'Prestige',
 
 
+}
+
+const SCORER_CODE = {
+    '12812770': '68537fab', // table 1
+    '12812773': '8041d95e', // table 2
+    '12812776': 'e241b2b7', // table 3
+    '12812779': '2bf6b7e8', // table 4
+    '12812782': '68a2b136', // table 5
+    '12812785': 'e41be679', // table 6
+    '12812797': 'de108f97', // table 7
+    '12812800': '93c9d835', // table 8
+    '12812803': '48d19094', // table 9
+    '12812806': 'e91a16ed', // table 10
+    '12812812': 'f9df0bac', // table 11
+    '12812815': '53bb9a74', // table 12
+    '12812818': '1e42635d', // table 13
+    '12812824': 'a3c6665d', // table 14
+    '12812827': '68354abc', // table 15
+    '12812830': 'eb66652a', // table 16
+    '12812833': '7f8283bc', // table 17
+    '12812839': 'aab530ea', // table 18
+    '12812842': 'b0d08428', // table 19
+    '12812845': '8891f133', // table 20
 }
 
 const tournaments = [
@@ -109,6 +132,9 @@ async function getProxy() {
             match.organization = labels[tournament.organization] ?? tournament.organization;
             match.draw = labels[tournament.draw] ?? tournament.draw;
             match.duration = 120
+            if (SCORER_CODE[match.tableId]) {
+                match.scorerUrl = 'https://cuescore.com/scoreboard/?code=' + SCORER_CODE[match.tableId];
+            }
         }
     }
     return proxy;
