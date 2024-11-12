cc28d776df6949b1303c969e08d4370f49034cd4
M96RU <m96ru.info@gmail.com>
2024-11-12 22:01:27 +0100

    Init fake TR3

--- cuescore/data/organizations.js
+++ cuescore/data/organizations.js
@@ -58,6 +58,10 @@ const organizations = [
                 id: 2,
                 label: "TR2",
                 location: "Bourg les Valence"
+            }, {
+                id: 3,
+                label: "TR3",
+                location: "Romans ou Feurs"
             }
         ],
         draws: [
