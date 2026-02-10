473e78150596651e8532eda2f119fbff7a7574e8
M96RU <m96ru.info@gmail.com>
2026-02-10 22:09:07 +0100

    LBARA 26 TR5 Results

--- cuescore/data/tournaments.js
+++ cuescore/data/tournaments.js
@@ -18,61 +18,61 @@ const tournaments = [
         organization: 'lbara',
         event: 5,
         draw: 'prestige',
-        live: true
+        live: false
     }, {
         id: 69037939,
         organization: 'lbara',
         event: 5,
         draw: 'excellence',
-        live: true
+        live: false
     }, {
         id: 74446591,
         organization: 'lbara',
         event: 5,
         draw: 'promotion',
-        live: true
+        live: false
     }, {
         id: 69038230,
         organization: 'lbara',
         event: 5,
         draw: 'women',
-        live: true
+        live: false
     }, {
         id: 69038281,
         organization: 'lbara',
         event: 5,
         draw: 'veteran',
-        live: true
+        live: false
     }, {
         id: 69039763,
         organization: 'lbara',
         event: 5,
         draw: 'jeune',
-        live: true
+        live: false
     }, {
         id: 74760010,
         organization: 'lbara',
         event: 5,
         draw: 'jeune_b',
-        live: true
+        live: false
     }, {
         id: 69034009,
         organization: 'lbara',
         event: 5,
         draw: 'prestige_bonus',
-        live: true
+        live: false
     }, {
         id: 69038092,
         organization: 'lbara',
         event: 5,
         draw: 'excellence_bonus',
-        live: true
+        live: false
     }, {
         id: 69041266,
         organization: 'lbara',
         event: 5,
         draw: 'promotion_bonus',
-        live: true
+        live: false
     },
 
 
